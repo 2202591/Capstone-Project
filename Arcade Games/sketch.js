b95915f1b88c2005b0df85748dcc90623fe9bf81
2202591 <2202591@spsd.sk.ca>
2025-12-03 14:10:47 -0600

    d

--- Arcade Games/sketch.js
+++ Arcade Games/sketch.js
@@ -4,7 +4,7 @@
 // CS30 Capstone Project
 // Mr. Scott
 
-let grid = [[]];
+let grid = [];
 
 let squareSize = 30;
 let rows =  10;
@@ -34,10 +34,9 @@ function randomGrid() {  //randomizes grid start
   for (let y = 0; y < rows; y++) {
     grid.push([]);
     for (let x = 0; x < cols; x++) { 
-      grid[y].push(new Detector(x,y,3));
+      grid[y].push(new DetectorOrMine(x,y,3));
     }
   } 
-  grid.pop();
 }
 
 function showGrid() {
@@ -56,33 +55,6 @@ function getCurrentX() {
 function getCurrentY() {  
   let constrainedY = constrain(mouseY, 0, height-1);
   return floor(constrainedY / squareSize);
-}
-
-function detectMines(x,y){
-  if(grid[y][x] === Mine){
-    return true;
-  }
-  else{
-    return false;
-  }
-}
-
-
-class Mine {
-  constructor(x,y) {
-    this.x = x;
-    this.y = y;
-    this.colorOne = color(238,230,199);
-    this.colorTwo = color(230, 59, 87);
-  }
-
-  display() {
-    fill(this.colorOne);
-    square(this.x*squareSize, this.y*squareSize, squareSize);
-    fill(this.colorTwo);
-    circle((this.x + 0.5)*squareSize, (this.y + 0.5)*squareSize, squareSize/2);
-  }
-
 }
 
 class DetectorOrMine {
@@ -118,19 +90,15 @@ class DetectorOrMine {
     }
   }
 
-  nearMines() {
-    for(let y = 0; y < rows; y++){
-      for(let x = 0; x < cols; x++){
-        // if(detectMines(x+1,y)) this.minesNear++;
-        // if(detectMines(x-1,y)) this.minesNear++;
-        // if(detectMines(x+1,y+1)) this.minesNear++;
-        // if(detectMines(x-1,y+1))this. minesNear++;
-        // if(detectMines(x,y+1)) this.minesNear++;
-        // if(detectMines(x,y-1)) this.minesNear++;
-        // if(detectMines(x-1,y-1)) this.minesNear++;
-        // if(detectMines(x+1,y-1)) this.minesNear++;
-      }
-    }
+  nearMines() {  // nodes distance squareSize +squarwSize*0.25
+    // if(grid[this.y+1][this.x].mine) minesNear++;
+    // if(grid[this.y+1][this.x-1].mine) minesNear++;
+    // if(grid[this.y+1][this.x+1].mine) minesNear++;
+    // if(grid[this.y-1][this.x].mine) minesNear++;
+    // if(grid[this.y-1][this.x-1].mine) minesNear++;
+    // if(grid[this.y-1][this.x+1].mine) minesNear++;
+    // if(grid[this.y][this.x+1].mine) minesNear++;
+    // if(grid[this.y][this.x-1].mine) minesNear++;
   }
 }
 
