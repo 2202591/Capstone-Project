63c86f8d03b65d66f5477c39a8439d2ac3addb4b
psalzy08 <1203573@spsd.sk.ca>
2025-12-03 14:09:57 -0600

    started snake game

--- Snake/sketch.js
+++ Snake/sketch.js
@@ -6,7 +6,7 @@
 let grid = []
 let rows = 15;
 let cols = 17;
-
+let squareSize = 20;
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
@@ -17,11 +17,25 @@ function draw() {
 }
 
 function createGrid(){
+  // creates the background grid
   for(let y= 0; y < rows; y++){
     for(let x = 0; x < cols; x++){
       square(x*squareSize,y*squareSize,squareSize);
+      if(grid[y][x] === 201){
+        fill(0,201,0);
+      }
+      else{
+        fill(0,220,0);
+      }
       
-      
+    }
+  }
+}
+function gridColors(){
+  //places colors of grid in every square
+  for(let y = 0; y < rows; y++){
+    for(let x = 0; x < cols; x++){
+      grid[y][x] = floor(random(201,220));
     }
   }
 }
