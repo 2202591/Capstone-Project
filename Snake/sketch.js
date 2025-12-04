0bd80a59152a8f1a7b61ec7edbf250454e0d7260
psalzy08 <1203573@spsd.sk.ca>
2025-12-04 12:19:53 -0600

    added apples and randomized them

--- Snake/sketch.js
+++ Snake/sketch.js
@@ -7,7 +7,8 @@ let grid = []
 let rows = 15;
 let cols = 17;
 let squareSize = 30;
-let snakeLength = 30;
+let snakeLength = 3;
+let xSpeed;
 function setup() {
   createCanvas(cols*squareSize, rows*squareSize);
 
@@ -18,7 +19,8 @@ function setup() {
         grid[y][x] = floor(random(0,2));
       }
   }
-  mySnake = new Snake(3,7);
+  mySnake = new Snake(4,7,5,0.2);
+  myApple = new Fruit(1);
 }
 
 function draw() {
@@ -26,6 +28,8 @@ function draw() {
   background(220);
   createGrid();
   mySnake.display();
+  mySnake.move();
+  myApple.display();
 }
 
 function createGrid(){
@@ -45,15 +49,31 @@ function createGrid(){
   }
 }
 class Snake{
-  constructor(x,y,c){
+  constructor(x,y,c,s){
     this.x = x; this.y = y;
-    this.c = c;
+    this.c = c; this.s = s;
   }
   display(){
     fill(62, 146, 199);
-    square(this.x*squareSize,this.y*squareSize, squareSize);
+    rect(this.x*squareSize,this.y*squareSize,squareSize*-snakeLength, squareSize);
   }
   move(){
-
+    xSpeed = this.s;
+    this.x += xSpeed;
+  }
+}
+class Fruit{
+  constructor(count){
+    this.x = round(random(cols));
+    this.y = round(random(rows));
+    this.count = count;
+    if(this.x >= cols) this.x = 16;
+    else if(this.x < 1) this.x = 1;
+    if(this.y >= rows) this.y = 14;
+    else if(this.y < 1) this.y = 1;
+  }
+  display(){
+    fill(255, 0, 0);
+    square(this.x*squareSize, this.y*squareSize, squareSize)
   }
 }
