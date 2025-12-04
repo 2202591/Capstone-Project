178ad04b04789b1b77162e86a93fde44d6865683
psalzy08 <1203573@spsd.sk.ca>
2025-12-03 20:55:55 -0600

    created grid for snake and began snake class

--- Snake/sketch.js
+++ Snake/sketch.js
@@ -6,36 +6,54 @@
 let grid = []
 let rows = 15;
 let cols = 17;
-let squareSize = 20;
+let squareSize = 30;
+let snakeLength = 30;
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(cols*squareSize, rows*squareSize);
+
+  //initialize the grid
+    for(let y = 0; y < rows; y++){
+      grid[y] = [];
+      for(let x = 0; x < cols; x++){
+        grid[y][x] = floor(random(0,2));
+      }
+  }
+  mySnake = new Snake(3,7);
 }
 
 function draw() {
+  noStroke();
   background(220);
   createGrid();
+  mySnake.display();
 }
 
 function createGrid(){
   // creates the background grid
   for(let y= 0; y < rows; y++){
     for(let x = 0; x < cols; x++){
-      square(x*squareSize,y*squareSize,squareSize);
-      if(grid[y][x] === 201){
-        fill(0,201,0);
+      grid[y][x] = 0;
+      
+      if((x+y)%2 === 0){
+        fill(199, 227, 113);
       }
       else{
-        fill(0,220,0);
+        fill(163, 199, 62);
       }
-      
+      square(x*squareSize,y*squareSize,squareSize);
     }
   }
 }
-function gridColors(){
-  //places colors of grid in every square
-  for(let y = 0; y < rows; y++){
-    for(let x = 0; x < cols; x++){
-      grid[y][x] = floor(random(201,220));
-    }
+class Snake{
+  constructor(x,y,c){
+    this.x = x; this.y = y;
+    this.c = c;
+  }
+  display(){
+    fill(62, 146, 199);
+    square(this.x*squareSize,this.y*squareSize, squareSize);
+  }
+  move(){
+
   }
 }
