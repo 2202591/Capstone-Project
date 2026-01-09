b3f2190a8589d9a4a44a71d668190600aa75f960
2202591 <2202591@spsd.sk.ca>
2026-01-09 14:09:26 -0600

    combined games (kinda)

--- Arcade Games/sketch.js
+++ Arcade Games/sketch.js
@@ -4,7 +4,8 @@
 // CS30 Capstone Project
 // Mr. Scott
 
-let grid = [];
+let gridMines = [];
+let gridSnakes = [];
 
 //mineweeper Variables
 let squareSize = 30;
@@ -25,6 +26,7 @@ function setup() {
   textAlign(CENTER, CENTER);
   textSize(20);
   createCanvas(cols*squareSize, rows*squareSize);
+  mineGrid();
   snakeGrid();
 }
 
@@ -32,28 +34,46 @@ function draw() {
   background(220);
   x = getCurrentX();
   y = getCurrentY();
-  start();
   if(game === 1) mineSweeper();
   if(game === 2) snake();
+  start();
 }
 
 // Start Screen
 function start() {
-  fill(0); 
-  square(width*0.33-50, height/2-50, 100);
-  square(width*0.66-50, height/2-50, 100);
-
-  if(mouseX > width*0.33-50 && mouseX < width*0.33+50) {
-    if(mouseY > height/2-50 && mouseY < height/2+50) {
-      if(mouseIsPressed) {
-        game = 1;
+  if(game === 0) {
+    fill(0);
+    square(width*0.33-50, height/2-50, 100);
+    square(width*0.66-50, height/2-50, 100);
+    fill(255)
+    text("MINESWEEPER", width*0.33 - 50, height/2);
+    text("SNAKE", width*0.66 - 50, height/2);
+    if(mouseX > width*0.33-50 && mouseX < width*0.33+50) {
+      if(mouseY > height/2-50 && mouseY < height/2+50) {
+        if(mouseIsPressed) {
+          game = 1;
+          mineGrid();
+        }
+      }
+    }
+    if(mouseX > width*0.66-50 && mouseX < width*0.66+50) {
+      if(mouseY > height/2-50 && mouseY < height/2+50) {
+        if(mouseIsPressed) {
+          game = 2;
+          snakeGrid();
+        }
       }
     }
   }
-  if(mouseX > width*0.66-50 && mouseX < width*0.66+50) {
-    if(mouseY > height/2-50 && mouseY < height/2+50) {
-      if(mouseIsPressed) {
-        game = 2;
+  else {
+    fill(255);
+    textSize(20);
+    text("BACK", width*0.94, height*0.03);
+    if(mouseX > width*0.85 && mouseX < width) {
+      if(mouseY > 0 && mouseY < height*0.05) {
+        if(mouseIsPressed) {
+          game = 0;
+        }
       }
     }
   }
@@ -61,44 +81,49 @@ function start() {
 
 // Minesweeper
 function mineSweeper() {
-  showGrid();
+  showGridM();
   checkGrid();
 }
 
 function mineGrid() {  //randomizes grid start
   for (let y = 0; y < rows; y++) {
-    grid.push([]);
+    gridMines.push([]);
+    for (let x = 0; x < cols; x++) {
+      gridMines[y].push(1);
+    }
+  }
+  for (let y = 0; y < rows; y++) {
     for (let x = 0; x < cols; x++) {
       let num = random(1);
       if(x < rows - 1 && x > 0 && y < cols - 1 && y > 0) { //creates grid inside the boarder
         if(num < 0.165) { // frequency of mine placement
-          grid[y].push(new DetectorOrMine(x,y,"mine")); //places mine
+          gridMines[y][x] = new DetectorOrMine(x,y,"mine"); //places mine
         }
-        else grid[y].push(new DetectorOrMine(x,y,"detector")); //places detector
+        else gridMines[y][x] = new DetectorOrMine(x,y,"detector"); //places detector
       }
       else {
-        grid[y].push(new DetectorOrMine(x,y,"boarder")); // places bparder
+        gridMines[y][x] = new DetectorOrMine(x,y,"boarder"); // places bparder
       }
       
     }
   } 
   for (let y = 0; y < rows; y++) { //checks every square to see how many mines are near
     for (let x = 0; x < cols; x++) { 
-      if(y > 0 && y < rows-1 && x < cols-1 && x > 0) grid[y][x].nearMines();
+      if(y > 0 && y < rows-1 && x < cols-1 && x > 0) gridMines[y][x].nearMines();
     }
   }
   for (let y = 0; y < rows; y++) { //reveals all numbers that are beside squares with no mines near
     for (let x = 0; x < cols; x++) { 
-      if(y > 0 && y < rows-1 && x < cols-1 && x > 0) grid[y][x].noMines();
+      if(y > 0 && y < rows-1 && x < cols-1 && x > 0) gridMines[y][x].noMines();
     }
   }
 
 }
 
-function showGrid() { //displays the grid
+function showGridM() { //displays the grid
   for (let y = 0; y < rows; y++) {
     for (let x = 0; x < cols; x++) { 
-      grid[y][x].display();
+      gridMines[y][x].display();
     }
   } 
 }
@@ -110,16 +135,16 @@ function checkGrid() { //checks how many mines, flags, and non mines
   flagCount = 0;
   for (let y = 0; y < rows; y++) {
     for (let x = 0; x < cols; x++) { 
-      if(grid[y][x].grass === false && grid[y][x].mine !== "mine") {
+      if(gridMines[y][x].grass === false && gridMines[y][x].mine !== "mine") {
         count++;  //counts how mamy squares have been clicked
       }
-      if(grid[y][x].mine === "mine") {
+      if(gridMines[y][x].mine === "mine") {
         mineCount++;  //counts # of mines
       }
-      if(grid[y][x].mine === "mine" && grid[y][x].grass === false){
+      if(gridMines[y][x].mine === "mine" && gridMines[y][x].grass === false){
         activeMine = 1;  //checks if a mines has been clicked
       }
-      if(grid[y][x].flag){
+      if(gridMines[y][x].flag){
         flagCount++;  //counts # of flags active
       }
     }
@@ -149,18 +174,18 @@ function youLose() {
 function mousePressed() {
   if(game === 1) {
     if (mouseX <= width & mouseY <= height) { //mouse must be in the canvas
-      if (grid[y][x].grass){
+      if (gridMines[y][x].grass){
         if(keyIsDown(17)) { //if ctrl is downn places flags if number of flags > 0
-          if(grid[y][x].flag === false){
-            if(mineCount - flagCount > 0) grid[y][x].flag = true;
+          if(gridMines[y][x].flag === false){
+            if(mineCount - flagCount > 0) gridMines[y][x].flag = true;
           }
           else {
-            grid[y][x].flag = false;  //removes flag if there is a flag already there
+            gridMines[y][x].flag = false;  //removes flag if there is a flag already there
           }
         }
         else {
-          if(grid[y][x].flag === false) { //allows to turnoer new squares if there is no flag
-            grid[y][x].grass = false;
+          if(gridMines[y][x].flag === false) { //allows to turnoer new squares if there is no flag
+            gridMines[y][x].grass = false;
           }
         }
       }
@@ -185,8 +210,10 @@ class DetectorOrMine {
     this.y = y;
 
     this.mineColor = color(230, 59, 87);  //colors for mine
-    this.colorOne = color(199, 227, 113);   //background colors
-    this.colorTwo = color(163, 199, 62);
+    this.grassOne = color(199, 227, 113);   //grass colors
+    this.grassTwo = color(163, 199, 62);
+    this.colorOne = color(238, 230, 199);   //grass colors
+    this.colorTwo = color(215, 207, 174);
     this.boarderColor = color(100); //boarder color
 
     this.flag = false;  //each square starts out as not showing and no flag
@@ -224,8 +251,8 @@ class DetectorOrMine {
     }
     else{
       if(this.flag) {  //makes a flag
-        if((this.x+this.y)%2 === 0) fill(199,227,113);
-        else fill(163,199,62);
+        if((this.x+this.y)%2 === 0) fill(this.grassOne);
+        else fill(this.grassTwo);
         if(this.mine === "boarder") fill(this.boarderColor);
         square(this.x*squareSize, this.y*squareSize, squareSize);
         
@@ -238,8 +265,8 @@ class DetectorOrMine {
         }
       }
       else {
-        if((this.x+this.y)%2 === 0) fill(199,227,113); //still keeps the checkerboard pattern
-        else fill(163,199,62);
+        if((this.x+this.y)%2 === 0) fill(this.grassOne); //still keeps the checkerboard pattern
+        else fill(this.grassTwo);
         if(this.mine === "boarder") fill(this.boarderColor);
         square(this.x*squareSize, this.y*squareSize, squareSize);
       }
@@ -247,26 +274,26 @@ class DetectorOrMine {
   }
 
   nearMines() { //checks how many mines are near each square in a one tile radius
-    if(grid[this.y-1][this.x].mine === "mine") this.minesNear++;
-    if(grid[this.y+1][this.x].mine === "mine") this.minesNear++;
-    if(grid[this.y][this.x-1].mine === "mine") this.minesNear++;
-    if(grid[this.y][this.x+1].mine === "mine") this.minesNear++;
-    if(grid[this.y-1][this.x-1].mine === "mine") this.minesNear++;
-    if(grid[this.y+1][this.x+1].mine === "mine") this.minesNear++;
-    if(grid[this.y+1][this.x-1].mine === "mine") this.minesNear++;   
-    if(grid[this.y-1][this.x+1].mine === "mine") this.minesNear++;
+    if(gridMines[this.y-1][this.x].mine === "mine") this.minesNear++;
+    if(gridMines[this.y+1][this.x].mine === "mine") this.minesNear++;
+    if(gridMines[this.y][this.x-1].mine === "mine") this.minesNear++;
+    if(gridMines[this.y][this.x+1].mine === "mine") this.minesNear++;
+    if(gridMines[this.y-1][this.x-1].mine === "mine") this.minesNear++;
+    if(gridMines[this.y+1][this.x+1].mine === "mine") this.minesNear++;
+    if(gridMines[this.y+1][this.x-1].mine === "mine") this.minesNear++;   
+    if(gridMines[this.y-1][this.x+1].mine === "mine") this.minesNear++;
   }
 
   noMines() {
     if(this.mine !== "mine") {  //displays all squares that are next to squares with no mines
-      if(grid[this.y-1][this.x].minesNear === 0 && grid[this.y-1][this.x].mine === "detector") this.grass = false;
-      if(grid[this.y+1][this.x].minesNear === 0 && grid[this.y+1][this.x].mine === "detector") this.grass = false;
-      if(grid[this.y][this.x-1].minesNear === 0 && grid[this.y][this.x-1].mine === "detector") this.grass = false;
-      if(grid[this.y][this.x+1].minesNear === 0 && grid[this.y][this.x+1].mine === "detector") this.grass = false;
-      if(grid[this.y-1][this.x-1].minesNear === 0 && grid[this.y-1][this.x-1].mine === "detector") this.grass = false;
-      if(grid[this.y+1][this.x+1].minesNear === 0 && grid[this.y+1][this.x+1].mine === "detector") this.grass = false;
-      if(grid[this.y+1][this.x-1].minesNear === 0 && grid[this.y+1][this.x-1].mine === "detector") this.grass = false;   
-      if(grid[this.y-1][this.x+1].minesNear === 0 && grid[this.y-1][this.x+1].mine === "detector") this.grass = false;
+      if(gridMines[this.y-1][this.x].minesNear === 0 && gridMines[this.y-1][this.x].mine === "detector") this.grass = false;
+      if(gridMines[this.y+1][this.x].minesNear === 0 && gridMines[this.y+1][this.x].mine === "detector") this.grass = false;
+      if(gridMines[this.y][this.x-1].minesNear === 0 && gridMines[this.y][this.x-1].mine === "detector") this.grass = false;
+      if(gridMines[this.y][this.x+1].minesNear === 0 && gridMines[this.y][this.x+1].mine === "detector") this.grass = false;
+      if(gridMines[this.y-1][this.x-1].minesNear === 0 && gridMines[this.y-1][this.x-1].mine === "detector") this.grass = false;
+      if(gridMines[this.y+1][this.x+1].minesNear === 0 && gridMines[this.y+1][this.x+1].mine === "detector") this.grass = false;
+      if(gridMines[this.y+1][this.x-1].minesNear === 0 && gridMines[this.y+1][this.x-1].mine === "detector") this.grass = false;   
+      if(gridMines[this.y-1][this.x+1].minesNear === 0 && gridMines[this.y-1][this.x+1].mine === "detector") this.grass = false;
     }
   }
 }
@@ -291,14 +318,14 @@ let snakes = [];
 // }
 
 function snake(){
-  
+  snakeGrid();
+  showGridS();
 }
 
-function addFruit(){
+function growSnake(){
   for(let f of fruits){
     f.display();
   }
-  growSnake();
   for(let f of fruits){
     for(let s of snakes){
       if(f.x * squareSize === s.x && f.y * squareSize === s.y){
@@ -307,6 +334,25 @@ function addFruit(){
         score++;
         print(score);
       }
+    }
+  }
+  if(f.x * squareSize === s.x && f.y * squareSize === s.y){
+    if(changeDir === 1) { // down
+      snakes.push(new Snake(backX, (backY+squareSize)));
+  
+    }
+    if(changeDir === 2) {// up
+      snakes.push(new Snake(backX, (backY-squareSize)));
+  
+    }
+    if(changeDir === 3) { //right
+      snakes.push(new Snake((backX-squareSize), backY));
+  
+  
+    }
+    if(changeDir === 4) {//left
+      snakes.push(new Snake((backX+squareSize), backY));
+  
     }
   }
 }
@@ -345,35 +391,6 @@ function addFruit(){
 //   text("Score:"+score, 0, squareSize)
 // }
 
-function growSnake(){
-  let l = snakes.length;
-  let backX = snakes[l-1].x
-  let backY = snakes[l-1].y
-  for(let f of fruits){
-    for(let s of snakes) {
-      if(f.x * squareSize === s.x && f.y * squareSize === s.y){
-        if(changeDir === 1) { // down
-          snakes.push(new Snake(backX, (backY+squareSize)));
-      
-        }
-        if(changeDir === 2) {// up
-          snakes.push(new Snake(backX, (backY-squareSize)));
-      
-        }
-        if(changeDir === 3) { //right
-          snakes.push(new Snake((backX-squareSize), backY));
-      
-      
-        }
-        if(changeDir === 4) {//left
-          snakes.push(new Snake((backX+squareSize), backY));
-      
-        }
-      }
-    } 
-  }
-}
-
 function turn() {
   let head = snakes[0];
 
@@ -407,44 +424,36 @@ function keyPressed() {
   } 
 }
 
-// function snakeGrid() {
-//   // creates the background grid
-//   for (let y = 0; y < rows; y++) {
-//     for (let x = 0; x < cols; x++) {
-//       if(x < rows - 1 && x > 0 && y < cols - 1 && y > 0) { //creates grid inside the boarder
-//         if ((x + y) % 2 === 0) { //every second square
-//           fill(199, 227, 113);
-//         }
-//         else {
-//           fill(163, 199, 62);
-//         }
-//       }
-//       else {
-//         fill(100);
-//       }
-//       square(x * squareSize, y * squareSize, squareSize);
-//     }
-//   }
-// }
+function snakeGrid() {
+  // creates the background grid
+  for (let y = 0; y < rows; y++) {
+    gridSnakes.push([]);
+    for (let x = 0; x < cols; x++) {
+      if(x < rows - 1 && x > 0 && y < cols - 1 && y > 0) { //creates grid inside the boarder
+        if ((x + y) % 2 === 0) { //every second square
+          gridSnakes[y].push("grassOne");
+        }
+        else {
+          gridSnakes[y].push("grassTwo");
+        }
+      }
+      else {
+        gridSnakes[y].push("boarder");
+      }
+    }
+  }
+}
 
-// function snakeGrid() {  //randomizes grid start
-//   for (let y = 0; y < rows; y++) {
-//     grid.push([]);
-//     for (let x = 0; x < cols; x++) {
-//       if(x < rows - 1 && x > 0 && y < cols - 1 && y > 0) { //creates grid inside the boarder
-//         if ((x + y) % 2 === 0) { //every second square
-//           fill(199, 227, 113);
-//         }
-//         else {
-//           fill(163, 199, 62);
-//         }
-//       }
-//       else {
-//         fill(100);
-//       }
-//     }
-//   } 
-// }
+function showGridS() {
+  for (let y = 0; y < rows; y++) {
+    for (let x = 0; x < cols; x++) { 
+      if(gridSnakes[y][x] === "grassOne") fill(199, 227, 113);
+      else if(gridSnakes[y][x] === "grassTwo") fill(163, 199, 62)
+      else fill(100);
+      square(x * squareSize, y * squareSize, squareSize);
+    }
+  } 
+}
 
 class Snake {
   constructor(x, y) {
@@ -485,8 +494,6 @@ class Snake {
       changeDir = 4;
     }
   }
-
-  // .
 }
 class Fruit {
   constructor(count) {
